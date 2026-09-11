3c40037b4c3279cd7f35ed9bca20aaf188f40bfa
Charles Renan S. Retes <workretes@gmail.com>
2026-09-11 13:20:02 +0800

    prepare for railway

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('qr_code')->unique()->nullable()->after('id');
+            $table->string('qr_code')->unique()->nullable();
             $table->string('status')->default('out');
             $table->timestamps();
         });
